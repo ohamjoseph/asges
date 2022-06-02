445b7397b0263b95af0aa7d84158d5fdd7330ea2
shado <ouilyh@gmail.com>
2022-06-02 15:03:41 +0000

    bug activité status2

---
+++ src/Traits/StatusActiviteTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait StatusTrait
+{
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $status;
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function PreStatus()
+    {
+        if (is_null($this->status)) {
+            $this->status = 'ACTIVE';
+        }
+    }
+}
